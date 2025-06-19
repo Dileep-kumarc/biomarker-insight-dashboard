989a95381b530d9c82e44417db52717c50c4674d
Dileep Kumar c <88617226+Dileep-kumarc@users.noreply.github.com>
2025-06-19 14:38:05 +0530

    fix: resolve all build errors and update dependencies

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ origins = [
     "https://biomarker-insight-dashboard.netlify.app",
     "http://127.0.0.1:3000",
     "http://127.0.0.1",
-    "https://fixed-workings.vercel.app",
+    "https://DK-workings.vercel.app",
 ]
 
 app.add_middleware(
